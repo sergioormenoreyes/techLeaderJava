c543edef6cc2f7110baa72532d365217b70ae024
sormenor <sergioreyes.everis@latam.com>
2020-09-06 12:56:04 -0300

    Change name of application base

--- techLead/src/main/java/com/techlead/SpringTechLeaderJavaApplication.java
+++ techLead/src/main/java/com/techlead/SpringTechLeaderJavaApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootHelloWorldApplication {
+public class SpringTechLeaderJavaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootHelloWorldApplication.class, args);
+		SpringApplication.run(SpringTechLeaderJavaApplication.class, args);
 	}
 }
